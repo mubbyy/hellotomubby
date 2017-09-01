7d8e9cf2b2342be3e2c680107a73638f99d1b364
Syed Bilal Hussain <syedbilalhussain168@gmail.com>
2017-09-01 16:40:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
-
 <!DOCTYPE html>
 <html >
 <head>
-	<title><?php 
-     	$a;
-     	$b;
-     	$sum;
-
-
-	 ?></title>
+	<title>Mubby's Git</title>
 </head>
 <body>
-<input type="name" name="vala" >
-<input type="name" name=valb>
-<input type="submit" name="button">
-<output><h1> <<?
-$a=$_POST['vala'];
-$b=$_POST['valb'];
-$sum=$a+$b;
-php echo $sum ?></h1></output>
+<form action="" method="post">
+	<input type="text" name="vala">
+	<input type="text" name="valb">
+	<input type="submit" name="submit">
+</form>
+<?php
+if(isset($_POST['submit'])) {
+	$a=$_POST['vala'];
+	$b=$_POST['valb'];
+	$sum=$a+$b;
+	echo $sum;
+}
+?>
 </body>
 </html>
